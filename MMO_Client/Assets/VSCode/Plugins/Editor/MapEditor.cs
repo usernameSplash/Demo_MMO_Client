99a8eb341f8948054c9518767cf79c19d2243a5f
usernameSplash <kkfenix3085@naver.com>
2020-09-06 00:46:13 +0900

    TileMap Collision, Unity Editor Customizing

---
+++ MMO_Client/Assets/VSCode/Plugins/Editor/MapEditor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class MapEditor : MonoBehaviour
+{
+
+#if UNITY_EDITOR
+
+    [MenuItem("Tools/GenerateMap %#g")]
+    private static void HelloWorld()
+    {
+        if (EditorUtility.DisplayDialog("Hello World", "Create?", "Create", "Cancel"))
+        {
+            new GameObject("Hello World");
+        }
+    }
+
+
+#endif
+
+}

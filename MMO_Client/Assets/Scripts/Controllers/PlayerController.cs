a682529823e74c8d1986eca37ba372e9cffedccb
usernameSplash <kkfenix3085@naver.com>
2020-09-06 23:06:22 +0900

    Player Moving

--- MMO_Client/Assets/Scripts/Controllers/PlayerController.cs
+++ MMO_Client/Assets/Scripts/Controllers/PlayerController.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    float _speed = 10.0f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += Vector3.up * Time.deltaTime * _speed;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position += Vector3.down * Time.deltaTime * _speed;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += Vector3.left * Time.deltaTime * _speed;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += Vector3.right * Time.deltaTime * _speed;
+        }
+
     }
 }

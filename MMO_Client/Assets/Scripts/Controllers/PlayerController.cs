eb5b665e6f497ba7bba07bf550884bc4b1d2a4cd
usernameSplash <kkfenix3085@naver.com>
2020-09-08 23:57:59 +0900

    Feat : Player Animation

--- MMO_Client/Assets/Scripts/Controllers/PlayerController.cs
+++ MMO_Client/Assets/Scripts/Controllers/PlayerController.cs
@@ -9,12 +9,77 @@ public class PlayerController : MonoBehaviour
     float _speed = 5.0f;
 
     Vector3Int _cellPos = Vector3Int.zero;
-    MoveDirection _moveDir = MoveDirection.None;
+    MoveDirection _moveDir = MoveDirection.Down;
+    Animator _animator;
+    MoveDirection Dir
+    {
+        get { return _moveDir; }
+        set
+        {
+            if (_moveDir == value)
+            {
+                return;
+            }
+            if (_isMoving == true)
+            {
+                return;
+            }
+            switch (value)
+            {
+                //Walking Animation
+                case MoveDirection.Up:
+                    _animator.Play("WALK_BACK");
+                    transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+                    break;
+                case MoveDirection.Down:
+                    _animator.Play("WALK_FRONT");
+                    transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+                    break;
+                case MoveDirection.Left:
+                    _animator.Play("WALK_RIGHT");
+                    //The player moves to the left, must be flipped horizontally.
+                    transform.localScale = new Vector3(-1.0f, 1.0f, 1.0f);
+                    break;
+                case MoveDirection.Right:
+                    _animator.Play("WALK_RIGHT");
+                    transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+                    break;
+
+                //Idle Animation
+                case MoveDirection.None:
+                    switch (_moveDir)
+                    {
+                        case MoveDirection.Up:
+                            _animator.Play("IDLE_BACK");
+                            transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+                            break;
+                        case MoveDirection.Down:
+                            _animator.Play("IDLE_FRONT");
+                            transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+                            break;
+                        case MoveDirection.Left:
+                            _animator.Play("IDLE_RIGHT");
+                            //The player looks the left, must be flipped horizontally.
+                            transform.localScale = new Vector3(-1.0f, 1.0f, 1.0f);
+                            break;
+                        case MoveDirection.Right:
+                            _animator.Play("IDLE_RIGHT");
+                            transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+                            break;
+                    }
+                    break;
+
+            }
+            _moveDir = value;
+        }
+    }
+
     bool _isMoving = false;
 
     // Start is called before the first frame update
     void Start()
     {
+        _animator = gameObject.GetComponent<Animator>();
         Vector3 pos = _grid.CellToWorld(_cellPos) + new Vector3(0.5f, 0.0f);
         transform.position = pos;
     }
@@ -25,36 +90,6 @@ public class PlayerController : MonoBehaviour
         GetDirectionInput();
         UpdateMovingStatus();
         UpdatePosition();
-    }
-
-    //
-    // Summary:
-    //     이동 가능한 상태일 때 실제 좌표를 이동
-    //     캐릭터 객체가 이동할 Grid 목표 위치 좌표
-    void UpdateMovingStatus()
-    {
-        if (!_isMoving)
-        {
-            switch (_moveDir)
-            {
-                case MoveDirection.None:
-                    break;
-                case MoveDirection.Up:
-                    _cellPos += Vector3Int.up;
-                    break;
-                case MoveDirection.Down:
-                    _cellPos += Vector3Int.down;
-                    break;
-                case MoveDirection.Left:
-                    _cellPos += Vector3Int.left;
-                    break;
-                case MoveDirection.Right:
-                    _cellPos += Vector3Int.right;
-                    break;
-            }
-            _isMoving = true;
-            _moveDir = MoveDirection.None;
-        }
     }
 
     //
@@ -84,33 +119,62 @@ public class PlayerController : MonoBehaviour
 
     }
 
+    // Summary:
+    //     이동 가능한 상태일 때 실제 좌표를 이동
+    //     캐릭터 객체가 이동할 Grid 목표 위치 좌표
+    void UpdateMovingStatus()
+    {
+        if (!_isMoving)
+        {
+            switch (Dir)
+            {
+                case MoveDirection.None:
+                    break;
+                case MoveDirection.Up:
+                    _cellPos += Vector3Int.up;
+                    break;
+                case MoveDirection.Down:
+                    _cellPos += Vector3Int.down;
+                    break;
+                case MoveDirection.Left:
+                    _cellPos += Vector3Int.left;
+                    break;
+                case MoveDirection.Right:
+                    _cellPos += Vector3Int.right;
+                    break;
+            }
+            _isMoving = true;
+        }
+    }
+
+
     //
     // Summary:
     //     키보드 입력을 통해 캐릭터의 이동방향을 결정하는 함수
     void GetDirectionInput()
     {
-        if (Input.anyKey)
+        if (Dir == MoveDirection.None)
         {
             if (Input.GetKey(KeyCode.W))
             {
-                _moveDir = MoveDirection.Up;
+                Dir = MoveDirection.Up;
             }
-            if (Input.GetKey(KeyCode.S))
+            else if (Input.GetKey(KeyCode.S))
             {
-                _moveDir = MoveDirection.Down;
+                Dir = MoveDirection.Down;
             }
-            if (Input.GetKey(KeyCode.A))
+            else if (Input.GetKey(KeyCode.A))
             {
-                _moveDir = MoveDirection.Left;
+                Dir = MoveDirection.Left;
             }
-            if (Input.GetKey(KeyCode.D))
+            else if (Input.GetKey(KeyCode.D))
             {
-                _moveDir = MoveDirection.Right;
+                Dir = MoveDirection.Right;
             }
         }
         else
         {
-            _moveDir = MoveDirection.None;
+            Dir = MoveDirection.None;
         }
     }
 }
